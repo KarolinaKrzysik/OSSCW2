b826e8074fd5ea11354ee660b4e7f180e4138915
Karolina Krzysik <79480965+KarolinaKrzysik@users.noreply.github.com>
2021-05-30 19:01:58 +0100

    Task 3- done without validation of delete request

--- deleteRecords.php
+++ deleteRecords.php
@@ -3,9 +3,23 @@
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
+   
+   echo template("templates/partials/header.php");
+   
+   if($_POST["deleteStudent"]){
 
-   echo "You have successfuly deleted following records: ";
-
-   echo template("templates/partials/footer.php");
+      echo "Student records: ";
+      foreach ($_POST['deleteStudent'] as $value){
+         
+         $sql2 = "DELETE FROM student WHERE studentid = $value";
+         $result = mysqli_query($conn, $sql2);
+         
+         echo "$value ";
+       }
+       echo 'have been successfuly deleted.';
+   }else{
+      echo '0 records have been deleted.';
+   }
+   echo template('templates/partials/footer.php');
 
 ?>
--- students.php
+++ students.php
@@ -4,39 +4,42 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
    $counter = 1;
-   $studentsDelete = array();
-
    echo template("templates/partials/header.php");
+?>
 
+<?php
    $sql = "SELECT studentid, dob, firstname, lastname, house, town, county, country, postcode FROM student";
    $result = mysqli_query($conn, $sql);
+// Confirm that the records are wished to be deleted when the form is submitted
 
-   //form with a checkbox column
-   echo "<form name = 'frmCheckboxes' action= 'deleteRecords.php' method = 'POST'>";
-
+  ?>
+   <form name = 'frmCheckboxes' action= 'deleteRecords.php' method = 'POST' 
+   onsubmit="return confirm(' Are you sure you want to delete these records?');">
+   <?php
+     //Make structure of the table, prepare headers
+      echo 
       $data['content'] .= "<table border='1'>";
       $data['content'] .= "<tr><th colspan='50' align='center'>Students Records</th></tr>";
       $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>Firstname</th><th>Lastname</th><th>House</th>
       <th>Town</th><th>County</th><th>Country</th><th>Postcode</th><th>Choose</th>
       </tr>";
-      // Display the modules within the html table
+      // Display the students in student table
 
-      
       while($row = mysqli_fetch_array($result)) {
         $data['content'] .= "<td>$row[studentid]</td><td> $row[dob] </td><td> $row[firstname] </td>";
         $data['content'] .= "<td> $row[lastname] </td><td> $row[house] </td><td> $row[town] </td>";
         $data['content'] .= "<td> $row[county] </td><td> $row[country] </td><td> $row[postcode]</td>
-        <td><input type='checkbox' id='$row[studentid]' name'deleteStudent[]' value='$row[studentid]'>
+        <td><input type='checkbox' id='$row[studentid]' name = 'deleteStudent[]' value='$row[studentid]'>
         <label for='student$counter'></label><br></td>
-        </tr>";   
+        </tr>";  
+        $counter++; 
       }
       $data['content'] .= "</table>";
-      
       echo template("templates/default.php", $data);
-      echo "<input type='submit' value='Delete Records' name = 'btnDelete'>";
-      echo "<input type = 'hidden' name = 'hiddenRecords' value = deleteStudent>";
+      echo "<input onclick='confirmAction()' type='submit' value='Delete Records' name = 'btnDelete'>";
       echo "</form>";
-
+   
+   
    echo template("templates/partials/footer.php");
 
-?>
+   ?>
